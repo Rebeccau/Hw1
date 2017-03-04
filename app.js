e97e0bcaeb1ce9e123b8e38d8cb0e4fa85b4630b
rebecca <ungar.rivka@gmail.com>
2017-03-04 22:11:35 +0200

    added fields and functionallity of checking how many items entered in input

---
+++ app.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular.module( 'LunchCheck', [] )
+  .controller( 'LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$injector = ['scope'];
+  function LunchCheckController( $scope ){
+    $scope.menu;
+    $scope.outputMessage;
+    $scope.checkMenu = function () {
+      var menu;
+      var numOfItems;
+      menu = $scope.menu
+      numOfItems = menu.split(',').length;
+
+      if( numOfItems <= 3 ){
+        $scope.outputMessage = "Enjoy!";
+      }else{
+        $scope.outputMessage = "Too much!";
+      }
+    }
+  }
+
+})();
